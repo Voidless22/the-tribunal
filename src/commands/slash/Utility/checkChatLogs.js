33dcbe63631118b8d2629a2504f53a285ad67436
Voidless <falant221@gmail.com>
2023-11-10 17:56:52 -0500

    Check Chat Logs command implemented. Uses memory buffer to send logs as a sorted and formatted text file. Can supply multiple characters.

--- src/commands/slash/Utility/checkChatLogs.js
+++ src/commands/slash/Utility/checkChatLogs.js
@@ -1,4 +1,4 @@
-const { ChatInputCommandInteraction, SlashCommandBuilder, ChannelType, EmbedBuilder, ActionRowBuilder, ButtonBuilder, PermissionsBitField } = require('discord.js');
+const { ChatInputCommandInteraction, SlashCommandBuilder, MessagePayload, ChannelType, AttachmentBuilder, EmbedBuilder, ActionRowBuilder, ButtonBuilder, PermissionsBitField } = require('discord.js');
 const ExtendedClient = require('../../../class/ExtendedClient');
 const db = require('../../../db')
 const config = require('../../../config');
@@ -17,27 +17,29 @@ module.exports = {
         const charName = interaction.options.getString('characters');
         const charNameArray = utils.seperateValues(charName).map(entry => entry.replace(/["\s,]/g, '')).filter(Boolean);
 
-        const results = await utils.queryChatLogs(charNameArray)
-  
-            let logs = [];
-            for(let i=0; i <= results.length ; i++) {
-                let timestamp = utils.formatDate(results[i].timerecorded);
-                let fromUser = results[i].from;
-                let toUser = results[i].to;
-                let messageContent = results[i].message
-                let fieldTitle = `**[${timestamp}] From: ${fromUser} To: ${toUser}**`
-                
-                logs.push(`[${fieldTitle}] ${messageContent}`)
-            }
+        let results = await utils.queryChatLogs(charNameArray)
+        console.log(results[0].timerecorded)
+        let logs = [];
+        for (let i = 0; i < results.length; i++) {
+            let timestamp = results[i].timerecorded;
+            let fromUser = results[i].from;
+            let toUser = results[i].to;
+            let messageContent = results[i].message;
+            let fieldTitle = `**[${timestamp}] From: ${fromUser} To: ${toUser}**`;
+
+            logs.push(`[${fieldTitle}] ${messageContent}`);
+        }
 
         logs = logs.reverse()
         let logsTxt = logs.join('\n');
-        
-        utils.writeToFileWithTiming(logsTxt, `${charName}-Chat-Logs.txt`);
+        const buffer = Buffer.from(logsTxt);
+        const attachment = new AttachmentBuilder()
+            .setFile(buffer, `${charName}-Chat-Logs.txt`)
+            .setName(`${charName}-chat-logs.txt`)
+
 
         if (results.length != 0) {
-            await interaction.channel.send({files: [`${charName}-Chat-Logs.txt`] })
-            utils.deleteFile(`${charName}-Chat-Logs.txt`);
+            await interaction.channel.send({ files: [attachment] })
             await interaction.reply("Success.")
 
         }
--- src/utils.js
+++ src/utils.js
@@ -42,7 +42,7 @@ async function confirmCharExists(charName) {
 
 async function queryChatLogs(charName) {
 
-    const sql = 'SELECT * FROM qs_player_speech WHERE `from` in (?) OR `to` in (?) ORDER BY timerecorded DESC';
+    const sql = 'SELECT * FROM qs_player_speech WHERE `from` in (?) OR `to` in (?) ORDER BY timerecorded DESC;'
     const values = [charName, charName];
 
     try {
@@ -53,47 +53,12 @@ async function queryChatLogs(charName) {
         throw error;
     }
 }
-function formatDate(inputDate) {
-    const date = new Date(inputDate); // Parse the input string into a Date object
 
-    // Extract individual components
-    const year = date.getFullYear();
-    const month = (date.getMonth() + 1).toString().padStart(2, '0'); // Months are zero-based, so add 1
-    const day = date.getDate().toString().padStart(2, '0');
-    const hour = date.getHours().toString().padStart(2, '0');
-    const minutes = date.getMinutes().toString().padStart(2, '0');
-
-    // Format the components as Month/Day/Year Hour:Minutes
-    const formattedDate = `${month}/${day}/${year} ${hour}:${minutes}`;
-
-    return formattedDate;
-}
 
 function seperateValues(values) {
     return values.split(/(?<![A-Za-z])|(?![A-Za-z])/);
 }
 
-function writeToFileWithTiming(data, filePath) {
-    // Record the start time
-    const startTime = new Date().getTime();
-  
-    // Write data to the file asynchronously
-    fs.writeFile(filePath, data, (err) => {
-      if (err) {
-        console.error('Error writing to file:', err);
-      } else {
-        // Record the end time
-        const endTime = new Date().getTime();
-  
-        // Calculate the time taken to write the file
-        const timeTaken = endTime - startTime;
-  
-        console.log(`Data has been written to the file in ${timeTaken} milliseconds.`);
-        
-
-    }
-    });
-  }
 
 async function createPetitionThread(threadName, petitionSection) {
     // If it does exist, create a new thread with the formatting of **submitting character** - **other characters involved** in the staff only section under the petition type channel
@@ -111,16 +76,7 @@ async function createPetitionThread(threadName, petitionSection) {
 
 }
 
-function deleteFile(filePath) {
-    // Delete the file
-    fs.unlink(filePath, (err) => {
-      if (err) {
-        console.error('Error deleting the file:', err);
-      } else {
-        console.log('File deleted successfully.');
-      }
-    });
-  }
+
 
 module.exports = {
     getStaffPetitionCategory: getStaffPetitionCategory,
@@ -131,7 +87,5 @@ module.exports = {
     confirmCharExists: confirmCharExists,
     seperateValues: seperateValues,
     queryChatLogs: queryChatLogs,
-    formatDate: formatDate,
-    writeToFileWithTiming:writeToFileWithTiming,
-    deleteFile:deleteFile
+
 }
